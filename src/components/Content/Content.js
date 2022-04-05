5f801382e39d1b6a12d50fcead734902c7d021fa
agiskorska <skorskaga@gmail.com>
2022-04-05 21:17:32 +0100

    Add colors to text in Content

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -8,9 +8,13 @@ const Content = () => {
         <Row className={styles.tree}>
             <Col style={{ paddingLeft: 10, paddingRight: 0 }}>src &gt; components &gt; Content &gt; JS Content.js &gt; ...</Col>
         </Row>
-        <Row>
-            <Col md={1}>1</Col>
-            <Col>import React from 'react';</Col>
+        <Row className={styles.text}>
+            <Col md={1}><span className={styles.gray}>1</span></Col>
+            <Col>
+            <p>
+              <span className={styles.purple}>import </span><span className={styles.blue}>React</span><span className={styles.purple}> from</span><span className={styles.orange}> 'react'</span><span className={styles.white}>;</span>
+            </p>
+            </Col>
         </Row>
         
     </Container>
