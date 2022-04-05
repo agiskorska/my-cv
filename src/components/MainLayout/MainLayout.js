d91f1686263c43e23c51d6a9174e65d61bb4e925
agiskorska <skorskaga@gmail.com>
2022-04-05 20:53:30 +0100

    Fix grid in MainLayout

--- src/components/MainLayout/MainLayout.js
+++ src/components/MainLayout/MainLayout.js
@@ -5,21 +5,22 @@ import Content from '../Content/Content';
 import QuickLinks from '../QuickLinks/QuickLinks';
 import { Container,  Row, Col } from 'react-bootstrap';
 import styles from './MainLayout.module.scss';
+import clsx from 'clsx';
 
 const MainLayout = () => {
   return (
-    <Container fluid style={{ paddingLeft: 0, paddingRight: 0 }}>
-      <Row className={styles.main} style={{ paddingLeft: 0, paddingRight: 0 }}>
-        <Col md={2} lg={3} className={styles.dissapear} style={{ paddingLeft: 0, paddingRight: 0 }}>
+    <Container fluid className={clsx('p-0')} >
+      <Row className={clsx(styles.main, 'p-0' )}>
+        <Col md={2} lg={2} className={clsx(styles.dissapear, 'p-0')} >
           <Row className={styles.sideRow} >
-            <Col className='d-sm-none' xs={1} sm={1} md={1} lg={1}><QuickLinks /></Col>
-            <Col xs={11} sm={11} md={11} lg={11}><SideBar /></Col>
+            <Col className={clsx('d-sm-none')} size='1'><QuickLinks /></Col>
+            <Col size='11'><SideBar /></Col>
           </Row>
         </Col>
-        <Col md={10} lg={9}>
-          <Row className={styles.content}>
+        <Col md={10} lg={10}>
+          <Row className={styles.content} >
             <Navigation />
-            <Col md={10}><Content /></Col>
+            <Col md={10} ><Content /></Col>
           </Row>
         </Col>
       </Row>
